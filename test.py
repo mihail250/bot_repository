e1ab2c45f6e464de41be519c6f39f2763908e06b
Mihail <mmishko01@gmail.com>
2023-10-09 08:03:07 +0300

    code from lesson, wich sends cats

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
-print('this is test')
-print('first change')
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = '6447210790:AAFVZydmJ39kX7pfQKAy5wHlAwj9bdquIJk'
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+
+offset = -2
+timeout = 10
+updates: dict
+
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True: 
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            do_something()
+
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
